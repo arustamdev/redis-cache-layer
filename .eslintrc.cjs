1708489d5c6dd899ff6994551d765e5534e8db0e
Rustam13223 <rustam13223@gmail.com>
2024-06-07 18:40:24 +0200

    eslintrc config fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
 };
