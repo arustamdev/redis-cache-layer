5cf9e3540ec68d7542840f1eab1c5814429ef1ae
arustamdev <arustamdev@gmail.com>
2024-06-08 17:15:18 +0200

    /products/bestReview added

---
+++ src/api/products/bestReview.ts
@@ -0,0 +1,39 @@
+import ProductModel from '../../models/Product';
+import { Request, Response, NextFunction } from 'express';
+
+async function bestReview(req: Request, res: Response, next: NextFunction) {
+  try {
+    const products = await ProductModel.aggregate([
+      {
+        $addFields: {
+          reviewsRating: { $avg: '$reviews.rating' },
+        },
+      },
+      {
+        $sort: { reviewsRating: -1 },
+      },
+      {
+        $limit: 3,
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          description: 1,
+          category: 1,
+          price: 1,
+          reviewsRating: 1,
+          discountPercentage: 1,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      products,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export default bestReview;
--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -4,8 +4,10 @@ const router = express.Router();
 
 import search from './search';
 import topDiscount from './topDiscount';
+import bestReview from './bestReview';
 
 router.get('/search', search);
 router.get('/topDiscount', topDiscount);
+router.get('/bestReview', bestReview);
 
 export default router;
